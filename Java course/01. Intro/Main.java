ba79022e74b7fbba2b20c599b7395690f73aee1e
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-07-08 09:16:07 +0300

    Create Main.java

---
+++ Java course/01. Intro/Main.java
@@ -0,0 +1,10 @@
+ /*
+  * Напишите программу, выводящую в консоль It's alive! It's alive!
+  * Класс должен называться "Main", это ограничение проверяющей системы.
+  */
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("It's alive! It's alive!");
+  }
+}
