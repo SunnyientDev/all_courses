b7ec73a968beb0ac35ed65292b67c73fced4f88e
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-09-07 14:15:54 +0300

    Create Part_2_CommandLine.java

---
+++ Java University course/Lab_1.Intro/Part_2_CommandLine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// package com.company.lesson2;
+
+public class Main 
+{
+    public static void main(String[] args) {
+      System.out.println("Checking");
+        for (String str : args) {
+            System.out.println("Command line: " + str);
+        }
+    }
+} 
