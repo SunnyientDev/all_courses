985ceeb94d9ac33331387748bb27f1a3da3f4edc
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-09-08 14:50:20 +0300

    Create Part_4_Dogs.java

---
+++ Java University course/Lab_2. OOP/Part_4_Dogs.java
@@ -0,0 +1,56 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Data_In = new Scanner(System.in);
+        System.out.print("Please, enter name and age of the dog \n");
+        System.out.print("Name: "); String Name1 = Data_In.nextLine();
+        System.out.print("Age: "); int Age1 = Data_In.nextInt();
+
+        Dog Dog1 = new Dog(Name1, Age1);
+        Dog1.PrintAge();
+        Dog1.PrintInfo1();
+
+        System.out.println("\n----------------------------------------------------");
+        //System.out.print("Please enter name and age of another dog \n");String Name2 = "Bobby";
+        String Name2 = "Bobby"; int Age2 = 3;
+        System.out.println("Name: " + Name2);
+        System.out.print("Age: " + Age2);
+
+        Dog Dog2 = new Dog();
+        Dog2.SetAge(Age2);
+        Dog2.SetName(Name2);
+        Dog2.PrintInfo2();
+
+        Dog[] Nursery = new Dog [] {Dog1, Dog2};
+    }
+}
+
+class Dog {
+    private String Name;
+    private int Age;
+
+    Dog(String m_Name, int m_Age) {
+        Name = m_Name;
+        Age = m_Age;
+    }
+
+    Dog(){
+
+    }
+
+    void SetName(String s_Name){ Name = s_Name; }
+    void SetAge(int s_Age){ Age = s_Age; }
+
+    void PrintAge(){ System.out.print("Age (human metrics): " + Age * 7 + " ages \n"); }
+    void PrintInfo1(){
+        System.out.println("--------------------Constructor---------------------");
+        System.out.printf("Information about dog: \nAge: %d\nName: " + Name, Age);
+    }
+
+    void PrintInfo2(){
+        System.out.println("\n----------------------Setters-----------------------");
+        System.out.printf("Information about dog: \nAge: %d\nName: " + Name, Age);
+    }
+}
