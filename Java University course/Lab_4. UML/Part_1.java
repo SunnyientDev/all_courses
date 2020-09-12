bcad26c470d7f90d69ef972e0a0628404908ed3e
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-09-12 23:11:14 +0300

    Create Part_1.java

---
+++ Java University course/Lab_4. UML/Part_1.java
@@ -0,0 +1,108 @@
+package com.company;
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle krug = new Circle(3.2,"Red",false);
+        Rectangle pram = new Rectangle(3.4,4.5,"Red",true);
+        Square kvad = new Square(4,"Blue",true);
+
+        System.out.print(krug.toString()+"\n"+ pram.toString()+"\n"+kvad.toString());
+    }
+}
+
+abstract public class Shape {
+    protected String Colour = "red";
+    protected boolean Filled = true;
+
+    public Shape(String Colour, boolean Filled){
+        this.Colour = Colour;
+        this.Filled = Filled;
+    }
+
+    public Shape(){}
+
+    public void setColour(){ this.Colour = Colour; }
+    public void setFilled(){ this.Filled = Filled; }
+    public String getColour(){ return Colour; }
+    public boolean isFilled(){ return Filled; }
+
+}
+
+public class Rectangle extends Shape{
+    double Width = 1;
+    double Length = 1;
+
+    public Rectangle(double Width, double Length, String Colour, boolean Filled){
+        this.Width = Width;
+        this.Length = Length;
+        this.Colour = Colour;
+        this.Filled = Filled;
+    }
+
+    public Rectangle(double Width, double Length){
+        this.Width = Width;
+        this.Length = Length;
+    }
+
+    public Rectangle(){}
+    public double getWidth(){ return Width; }
+    public double getLength(){ return Length; }
+    public double getArea(){ return Length*Width; }
+    public double getPerimeter(){ return (Length+Width)*2; }
+
+    public void setWidth(){ this.Width = Width; }
+    public void setLength(){ this.Length = Length; }
+
+    public String toString(){ return "Sth"; }
+}
+
+public class Circle extends Shape{
+    double Radius = 1;
+
+    public Circle(double Radius, String Colour, boolean Filled){
+        this.Width = Width;
+        this.Length = Length;
+        this.Colour = Colour;
+        this.Filled = Filled;
+    }
+
+    public Circle(double Radius){
+        this.Width = Width;
+        this.Length = Length;
+    }
+
+    public Circle(){}
+
+    public double getRadius(){ return Radius; }
+    public double getArea(){ return Math.PI * Radius; }
+    public double getPerimeter(){ return 2* Math.PI*Radius; }
+
+    public void setRadius(){ this.Radius = Radius; }
+    public String toString(){ return "Sth"; }
+}
+
+public class Square extends Rectangle{
+    public Square(){}
+
+    public Square(double Side) {
+        this.Length = Side;
+        this.Width = Side;
+    }
+
+    public Square(double Side, String Colour, boolean Filled) {
+        this.Length = Side;
+        this.Colour = Colour;
+    }
+
+    public void setSide(double Side){
+        this.Length = Side;
+        this.Width = Side;
+    }
+
+    public double getSide(){ return Length; }
+    public void setWidth(double Width){ this.Width = Width; }
+    public void setLength(double Length){ this.Length = Length; }
+    public String toString(){ return "Sth"; }
+}
