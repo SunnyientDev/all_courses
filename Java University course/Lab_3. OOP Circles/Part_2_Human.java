e64b885c03a5545eab36858f93513537cd5b987c
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-09-14 15:14:23 +0300

    Update Part_2_Human.java

--- Java University course/Lab_3. OOP Circles/Part_2_Human.java
+++ Java University course/Lab_3. OOP Circles/Part_2_Human.java
@@ -1,20 +1,43 @@
 package com.company;
 import java.util.Scanner;
 
-public class Human {
+public class Main {
     public static void main(String[] args) {
         Scanner Data_In = new Scanner(System.in);
 
         Head Head1 = new Head(); Head1.Setter("Blue", 20, 57);
         Leg Leg1 = new Leg(); Leg1.Setter(122, 20);
         Hand Hand1 = new Hand(); Hand1.Setter(40, 14);
-        
         Leg Leg2 = new Leg(); Leg2.Setter(122, 20);
         Hand Hand2 = new Hand(); Hand2.Setter(40, 14);
 
-        Head1.HeadInfo();
-        Leg1.LegsInfo();
-        Hand1.HandsInfo();
+        //Head1.HeadInfo();
+        //Leg1.LegsInfo();
+        //Hand1.HandsInfo();
+
+        Human OneHuman = new Human(Head1, Leg1, Leg2, Hand1, Hand2);
+        OneHuman.GetInfo();
+    }
+}
+
+class Human {
+    //properties
+    Head OneHead;
+    Leg LeftLeg; Leg RightLeg;
+    Hand LeftHand; Hand RightHand;
+
+    Human(Head OneHead, Leg LeftLeg, Leg RightLeg, Hand LeftHand, Hand RightHand){
+        this.OneHead = OneHead;
+        this.LeftHand = LeftHand;
+        this.LeftLeg = LeftLeg;
+        this.RightHand = RightHand;
+        this.RightLeg = RightLeg;
+    }
+
+    void GetInfo(){
+        OneHead.HeadInfo(); System.out.print("\n");
+        LeftLeg.LegsInfo(); System.out.print("\n");
+        LeftHand.HandsInfo();
     }
 }
 
