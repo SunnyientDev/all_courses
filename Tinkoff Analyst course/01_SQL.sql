b34a3aa9537a6708f0aea0dc47c824fbf76cd8c2
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2021-03-21 11:49:03 +0300

    Update 01_SQL.sql

--- Tinkoff Analyst course/01_SQL.sql
+++ Tinkoff Analyst course/01_SQL.sql
@@ -2,7 +2,9 @@ SELECT
 	a.partner_rk
 FROM
 	msu_analytics.partner a
-	inner join msu_analytics.location b
+	left join msu_analytics.location b
 		on a.partner_rk = b.partner_rk
+WHERE
+	b.partner_rk is NULL
 GROUP BY
 	1
