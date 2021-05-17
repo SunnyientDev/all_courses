0f11822e441d5a450dcb657d837f9c3273697515
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2021-05-17 16:19:13 +0300

    Add files via upload

---
+++ Python_course/Homeworks - Main course/03. Practice 2/2.1.py
@@ -0,0 +1,29 @@
+def f21(x):
+  if(x[1] == 1996):
+    if(x[0] == 2016):
+      if(x[3] == 1964):
+        return 0
+      elif(x[3] == 1969):
+        return 1
+    elif(x[0] == 1976):
+      return 2
+    elif(x[0] == 1999):
+      return 3
+
+  elif(x[1] == 1987):
+    if(x[2] == 'xslt'):
+      if(x[0] == 2016):
+        return 4
+      elif(x[0] == 1976):
+        return 5
+      elif(x[0] == 1999):
+        return 6
+    elif(x[2] == 'hy'):
+      if(x[0] == 2016):
+        return 7
+      elif(x[0] == 1976):
+        return 8
+      elif(x[0] == 1999):
+        return 9
+    elif(x[2] == 'volt'):
+      return 10
